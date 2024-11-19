f777819debf063b4f2ba142f11ad8a514958f74c
anishossain515 <amirul447700@gmail.com>
2024-11-19 23:52:55 +0600

    Video section done

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Navbar } from './Components/Navbar'
 import Hero from './Components/Hero'
 import Presentation from './Components/presentation'
 import Slider from './Components/Slider/Slider'
+import Video from './Components/video'
 function App() {
 
   return (
@@ -11,6 +12,7 @@ function App() {
      <Hero/>
      <Presentation/>
      <Slider/>
+     <Video/>
     </>
   )
 }
---
+++ src/Components/video.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+function Video() {
+  return (
+    <>
+      <section className='mt-[130px] bg-[#f9f9f9]  py-24 px-12'>
+
+        <div>
+
+          <h1 className='font-inter font-medium text-[55px] text-center'>Use illustrations in UI design</h1>
+
+          <div className='flex items-center justify-center pt-[54px]'>
+            <img src="/video.png" alt="video" />
+          </div>
+      
+
+        </div>
+
+      </section>
+    </>
+  )
+}
+
+export default Video
